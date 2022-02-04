7437c9e29fdc19e9d5f9ab3660c34dcf77ebb3fc
Timofti Gabriel <gabitim470@gmail.com>
2022-02-04 19:59:58 +0200

    Init repo

---
+++ src/test/java/com/pos/orders/Orders/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos.orders.Orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
